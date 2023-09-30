d3916668321e2941b5d295bcb0ed19bc41611cab
AzureAD\VanDangThai <van.dang@aswhiteglobal.com>
2023-10-01 01:36:22 +0700

    change the way of triggering point showing.

--- scripts/scrum-poker.js
+++ scripts/scrum-poker.js
@@ -1,96 +1,137 @@
-// Show the pocker's point
-const RegisterPockerPoints = () => {
-    if (!window.location.href.includes('scrumpoker-online.org'))
-        return;
+class Vote {
+    constructor(elVote) {
+        this._attributes = {
+            processed: 'processed',
+            voterName: 'voter-name'
+        };
 
-    setInterval(ShowPockerPoints, 1000);
-}
+        this._element = elVote;
+        this._elVoter = this._element.querySelector('td[class*=mat-column-displayName]');
+        this._elPoint = this._element.querySelector('div[class*=flip-card-back]').querySelectorAll(':scope > span')[0];
 
-const ShowPockerPoints = () => {
-    const PockerMagic = 'pocker-magic';
-
-    const isShowPointConfig = localStorage.getItem('showpointtoggle') == 'true' ? true : false;
-
-    //
-    // Add a button to quickly hide/show points
-    //
-
-    const showPointToggleElement = document.querySelector('.results-title div input[type="checkbox"]')
-    if (showPointToggleElement == null) {
-        const resultTitleElement = document.querySelector('.results-title span');
-        if (resultTitleElement == null)
-            return;
-
-        const showPointToggle = document.createElement("input");
-        showPointToggle.type = "checkbox";
-            showPointToggle.checked = isShowPointConfig;
-
-        showPointToggle.addEventListener('click', () => {
-            localStorage.setItem('showpointtoggle', showPointToggle.checked ? 'true' : 'false');
-            window.location.reload();
-        });
-
-        const containerElement = document.createElement("div");
-        containerElement.appendChild(resultTitleElement.cloneNode(true));
-        containerElement.appendChild(showPointToggle);
-
-        resultTitleElement.parentNode.replaceChild(containerElement, resultTitleElement);
+        if (!this._element.hasAttribute(this._attributes.processed)) {
+            this._elVoter.setAttribute(this._attributes.voterName, this._elVoter.innerText);
+        }
+        this._element.setAttribute(this._attributes.processed, null);
     }
 
-    //
-    // Retrieve and show points
-    //
-
-    const voteGroup = document.querySelector('tbody[role="rowgroup"]');
-
-    if (voteGroup === null)
-        return;
-
-    const votes = voteGroup.childNodes;
-
-    let highestPoint = {
-        nodes: [],
-        point: -1
+    getPoint() {
+        const point = parseFloat(this._elPoint.innerText);
+        return isNaN(point) ? null : point;
     }
 
-    votes.forEach(vote => {
-        if (vote.nodeName === '#comment')
-            return;
+    getVoterName() {
+        return this._elVoter.getAttribute(this._attributes.voterName);
+    }
 
-        if (vote.hasAttribute(PockerMagic))
-           return;
+    setVoterDisplayName(displayName) {
+        this._elVoter.innerText = displayName;
+    }
 
-        const paticipantNode = vote.querySelector('td[class*=mat-column-displayName]');
-        const pointNode = vote.querySelector('div[class*=flip-card-back]').querySelectorAll(':scope > span')[0];
+    highlight() {
+        this._elVoter.style.color = '#ff0000';
+        this._elVoter.style.fontWeight = 600;
+    }
 
-        const pointRaw = parseFloat(pointNode.innerText);
-        const point = isNaN(pointRaw) ? '-' : pointRaw;
+    unHighlight() {
+        this._elVoter.style.color = null;
+        this._elVoter.style.fontWeight = 'normal';
+    }
 
-        if (isShowPointConfig) {
-            paticipantNode.innerText = paticipantNode.innerText + ' (' + point + ')';
-        } else {
-            paticipantNode.innerText = paticipantNode.innerText;
-        }
+    static getVotes(doc) {
+        let elVotes = doc.querySelector('tbody[role="rowgroup"]')?.childNodes;
+        if (elVotes === null || elVotes === undefined) return;
 
-        if (point !== '-') {
-            if (highestPoint.point < point) {
-                highestPoint.nodes = [];
-                highestPoint.point = point;
-            }
+        // Convert NoteList to ChildNode[]
+        elVotes = Array.from(elVotes);
 
-            if (highestPoint.point == point) {
-                highestPoint.nodes.push(vote);
-            }
-        }
+        // Only TR rows are actually votes.
+        elVotes = elVotes.filter(elVote => elVote.nodeName === 'TR');
 
-        vote.setAttribute(PockerMagic, '');
-    });
-
-    if (isShowPointConfig) {
-        highestPoint.nodes.forEach(vote => {
-            vote.style.fontWeight = 600;
-        });
+        return elVotes.map(elVote => new Vote(elVote));
     }
 }
 
-RegisterPockerPoints();
+class ScrumPoker {
+    constructor(doc) {
+        this._intervalHandle = null;
+        this._document = doc;
+    }
+
+    highlightTopPoints(votes, enabled) {
+        const topPoint = {
+            votes: [],
+            value: -1
+        }
+
+        votes.forEach(vote => {
+            const point = vote.getPoint();
+            if (point === null) return;
+
+            if (topPoint.value < point) {
+                topPoint.votes = [];
+                topPoint.value = point;
+            }
+
+            if (topPoint.value === point) {
+                topPoint.votes.push(vote);
+            }
+        });
+
+        topPoint.votes.forEach(vote => {
+            if (enabled) {
+                vote.highlight();
+            } else {
+                vote.unHighlight();
+            }
+        });
+    }
+
+    showPoints(votes, enable) {
+        votes.forEach(vote => {
+            if (enable) {
+                const point = vote.getPoint() ?? ' ';
+                const displayName = point + ' - ' + vote.getVoterName();
+                vote.setVoterDisplayName(displayName);
+            } else {
+                vote.setVoterDisplayName(vote.getVoterName());
+            }
+        });
+    }
+
+    runHack(doc, enabled) {
+        const votes = Vote.getVotes(doc);
+
+        if (votes !== null) {
+            this.highlightTopPoints(votes, enabled);
+            this.showPoints(votes, enabled);
+        }
+    }
+
+    run() {
+        if (!window.location.href.includes('scrumpoker-online.org'))
+            return;
+
+        const fnKeyDown = (e) => {
+            if (e.key === 'Control') {
+                if (this._intervalHandle === null) {
+                    this._intervalHandle = setInterval(() => this.runHack(this._document, true), 100);
+                }
+            }
+        }
+        this._document.addEventListener("keydown", fnKeyDown);
+
+        const fnKeyUp = (e) => {
+            if (e.key === 'Control') {
+                clearInterval(this._intervalHandle);
+                this._intervalHandle = null;
+
+                this.runHack(this._document, false);
+            }
+        }
+        this._document.addEventListener("keyup", fnKeyUp);
+    }
+}
+
+const scrumPoker = new ScrumPoker(document);
+scrumPoker.run();
