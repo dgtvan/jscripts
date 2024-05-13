d2187374c5da89bc77f5a9c9c1ea09f2c594bc8c
Van Dang <dgtvan@gmail.com>
2024-05-13 10:08:43 +0700

    improve browser-stack.js

--- scripts/browser-stack.js
+++ scripts/browser-stack.js
@@ -2,12 +2,22 @@
 // Show pocker point
 //**************************************
 
+RegisterTotalStoryPointCalculation();
+
 const FixBrowserStackUI = () => {
     const packageListView = document.getElementById('app-testing-content');
     packageListView.style.width = '470px';
 }
 
 RegisterFixBrowserStackUI = () => {
+    if 
+    (
+        !window.location.href.includes('https://app-live.browserstack.com/') 
+    ) 
+    {
+        return;
+    }
+    
     setInterval(FixBrowserStackUI, 1000);
 }
 
